a6714eb0c1e46b82c3b2ff8d707514a1b31edaca
Maksim Andreev <Dayman88@yandex.ru>
2025-07-14 21:35:48 +0300

    v 0.4

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/js/main.js", // путь к js (пусть даже пустой)
+  output: {
+    filename: "js/main.js", // имя итогового js
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html", // твой html-шаблон
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "src/images", to: "images" },
+        { from: "src/css", to: "css" },
+      ],
+    }),
+  ],
+  devServer: {
+    static: "./dist",
+    open: true,
+  },
+};
